b5a3fd465fd48afd096c860b169a5630623c24f6
liyongbin <wb.liyongbin@mesg.corp.netease.com>
2018-07-17 21:35:17 +0800

    删除缓存

--- app.js
+++ app.js
@@ -49,31 +49,6 @@ router.get('/404', (ctx, next) => {
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-// app.use(async(ctx)=>{
-//   if(ctx.url==='/' && ctx.method==='GET'){
-//       //显示表单页面
-//       let html=`
-//           <h1>JSPang Koa2 request POST</h1>
-//           <form method="POST" action="/">
-//               <p>userName</p>
-//               <input name="userName" /><br/>
-//               <p>age</p>
-//               <input name="age" /><br/>
-//               <p>website</p>
-//               <input name="webSite" /><br/>
-//               <button type="submit">submit</button>
-//           </form>
-//       `;
-//       ctx.body=html;
-//   }else if(ctx.url==='/' && ctx.method==='POST'){
-//        let postData= ctx.request.body
-//        ctx.body=postData;
-//   }else{
-//       ctx.body='<h1>404!</h1>';
-//   }
-
-// });
-
 app.listen(9991, () => {
   console.log('koa2 已经开启')
 });
